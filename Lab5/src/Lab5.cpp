8874f4d5e2be86fa3c9f655afe0cf9dce8aad8e4
mhotrada <matthotradat@gmail.com>
2014-11-06 09:35:52 -0500

    hi

---
+++ Lab5/src/Lab5.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : Lab5.cpp
+// Author      : MH
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+#include "myfunctions5.h"
+
+int main(int argc, char* argv[]) {
+
+
+
+	return 0;
+}
