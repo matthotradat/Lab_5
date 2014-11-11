3bec3db17a8822c63ab6248a8cd98d7880fdbec8
matthotradat <matthotradat@gmail.com>
2014-11-10 23:36:46 -0500

    Update Lab5.cpp

--- Lab5/src/Lab5.cpp
+++ Lab5/src/Lab5.cpp
@@ -8,13 +8,25 @@
 
 #include <iostream>
 #include <string>
+#include <queue>
 #include <iomanip>
 using namespace std;
 #include "myfunctions5.h"
 
 int main(int argc, char* argv[]) {
 
-
+	int count=0;
+	int option;
+	
+	//Call your queue here
+	
+	cout << "[Obsterical Triage Patient Register]" << endl;
+	cout << "Please select an option below:" << endl;
+	//list the options/functions of your queuve in a cout here
+	
+	//cin >> option;
+	
+	//use a switch case to allow user to cycle through the options
 
 	return 0;
 }
