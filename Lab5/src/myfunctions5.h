a8c0ac4e61b4cd70cb1f56b8c2f94ca144197425
matthotradat <matthotradat@gmail.com>
2014-11-11 22:59:29 -0500

    Update myfunctions5.h

--- Lab5/src/myfunctions5.h
+++ Lab5/src/myfunctions5.h
@@ -6,8 +6,11 @@
  */
 #include <iostream>
 #include <string>
-#include <iomanip>
+#include <fstream>
+#include <sstream>
 #include <queue>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
 
 #ifndef MYFUNCTIONS5_H_
@@ -17,38 +20,83 @@ using namespace std;
 
 	class PatientData {
 
-	private:
+	public:
 		condition state;
 		int bednumber;
 		int contraction;
 		int dilation;
-	public:
+
 		string firstname;
 		string lastname;
 		int healthcard;
-		PatientData patient1;
-		~PatientData patient2;
+		PatientData();
+	    ~PatientData();
+
 	};
 
 	class PatientRegister {
 
 	public:
-		int SIZE;
-		//PatientData PatientQueue[SIZE];
-		queue<PatientData> PatientQueue[SIZE];
-		PatientRegister (); //Constructor
-		~PatientRegister (); //Destructor
-		
-		void AddPatient ()
-		{
-			
+		int size;
+		queue<PatientData> PatQueue;
+
+		void AddPatient (string line, PatientData Patient){
+
+			cin.ignore();
+			getline(cin,line); //get line from user
+
+			//create stringstream object
+			stringstream str(line);
+
+			str >> Patient.firstname;
+			str >> Patient.lastname;
+			str >> Patient.healthcard;
+
+			PatQueue.push(Patient);
 		}
-		
-		void DisplayaPatient()
-		{
-			
+
+		void ExaminePatient(PatientData Patient){
+			//PatQueue.front();
+			//PatQueue.pop();
+			if (Patient.state == UNKNOWN){
+				Patient.state = static_cast<condition>(rand() % 6);
+				PatQueue.front() = PatQueue.back();
+			}
+			else if (Patient.state == LABOUR){
+				cout << "Please enter the cervical dilation: ";
+				cin >> Patient.dilation;
+				cout << "Please enter the contraction rate: ";
+				cin >> Patient.contraction;
+				PatQueue.front() = PatQueue.back();
+			}
+			else if (Patient.state == CHECKUP){
+				Patient.dilation = 0;
+				PatQueue.front() = PatQueue.back();
+			}
+			else if ((Patient.state == LABOUR) && (Patient.dilation >= 4) && (Patient.contraction < 4)){
+				Patient.state = ADMIT;
+				PatQueue.pop();
+			}
+			else if (Patient.state == ADMIT){
+				cout << "Here are the Patient's Details:" << endl;
+				//cout << PatQueue.front(); //error
+				cout << "\nThe Patient has been admitted to birthing unit.";
+				PatQueue.pop();
+			}
+			//Need a condition for TEST condition, dont know what though
+			else if (Patient.state == RELEASE){
+				//cout << PatQueue.front(); //error
+				PatQueue.pop();
+			}
 		}
-	
+		void DisplayPatients(PatientData Patient){
+			//while (! PatQueue.empty()){
+			PatQueue.front();
+			PatQueue.pop();
+		}
+		PatientRegister();
+		~PatientRegister();
+
 	};
 
 #endif /* MYFUNCTIONS5_H_ */
